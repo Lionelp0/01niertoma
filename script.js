d23c352b85f59124c22a79fa3f994598d0ae9b5d
Lionelp0 <125425598+Lionelp0@users.noreply.github.com>
2023-02-22 13:17:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -30,22 +30,22 @@ jwplayer("container").setup({
 		     sources: [{
 		     {
           file:
-            "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/65f033fc-a2c4-44ad-8c1e-3fe081b6af80-480.m3u8lender_Studio.webm.1080p.webm",
+            "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/65f033fc-a2c4-44ad-8c1e-3fe081b6af80-480.m3u8",
           label: "480p",
           default: true
         },
         {
           file:
-            "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/857470f0-85b7-45df-838c-9cf001451114-240.m3u8ovie_by_Blender_Studio.webm.720p.webm",
+            "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/857470f0-85b7-45df-838c-9cf001451114-240.m3u8",
           label: "240p"
         },
         {
           file:
-            "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/65f033fc-a2c4-44ad-8c1e-3fe081b6af80-480.m3u8lender_Studio.webm.480p.webm",
+            "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/65f033fc-a2c4-44ad-8c1e-3fe081b6af80-480.m3u8",
           label: "480p"
         }, {
           file:
-            "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/f20ebbe9-5619-4870-927b-8e5e4bdecb66-720.m3u8lender_Studio.webm.1080p.webm",
+            "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/f20ebbe9-5619-4870-927b-8e5e4bdecb66-720.m3u8",
           label: "720p",
           default: true
         },		    
