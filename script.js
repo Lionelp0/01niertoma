fa8f65f4565963c90c6b31ea28b80329f10a245f
Lionelp0 <125425598+Lionelp0@users.noreply.github.com>
2023-02-22 14:24:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ jwplayer("container").setup({
 		    displaytitle: true,
 		    fullscreen: "false",
 		    primary: 'html5',
-		    stretching: "exactfit",
+		  
 		    autostart: true,
 
 		    skin: {
@@ -26,41 +26,27 @@ jwplayer("container").setup({
        {        
 		    title: "  ",
 		    description: "  ",
-		    image: "https://cdn.vietsubtv.org/images/film/truyen-ky-ly-tieu-long-5726.jpg",
+		    image: " ",
 		    sources: [{
-		    file: "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/857470f0-85b7-45df-838c-9cf001451114-240.m3u8",
+		    file: "https://peertube.otakufarms.com/static/webseed/b7f6e081-020e-4a23-b388-fb08d4228043-240.mp4",
 		      label: '240p',
 		      'type': 'mp4',
 		      primary: 'html5',
 
 		    },{
-		    file: "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/65f033fc-a2c4-44ad-8c1e-3fe081b6af80-480.m3u8",
+		    file: "https://peertube.otakufarms.com/static/webseed/9202c80c-facc-47d2-bb35-86fb09ae3614-480.mp4",
 		      label: '480p',
 		      'type': 'mp4',
 		      primary: 'html5',
 
 
 		    },{
-		     file: "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/f20ebbe9-5619-4870-927b-8e5e4bdecb66-720.m3u8",
+		     file: "https://peertube.otakufarms.com/static/webseed/a2202d14-6e76-42b3-96d0-9715e00fd77a-720.mp4",
 		      label: '720p',
 		      'type': 'mp4',
 		      primary: 'html5',
 		    }],
-		    captions: [{
-		      file: " ",
-		      label: ' ',
-		      kind: "captions",
-		      "default": true,
-		    },{ 
-		      file: " ",
-		         label: ' ',
-		         kind: "captions",
-		         },{ 
-		      file: " ",
-		         label: ' ',
-		         kind: "captions",
-		         }
-		    ],
+		    
 		    
 		 
 		  }//end of movies
@@ -76,4 +62,4 @@ jwplayer("container").setCaptions({
   "windowOpacity": 0,
   "color": "#ffff00"
 });
-	
+		
