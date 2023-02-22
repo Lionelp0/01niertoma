84ad2181f2f7da4e289e9e7293a8fd910417899a
Lionelp0 <125425598+Lionelp0@users.noreply.github.com>
2023-02-22 13:03:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -27,10 +27,9 @@ jwplayer("container").setup({
 		    title: "   ",
 		    description: "   ",
 		    image: "   ",
-		    sources: [{
-		    file: "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/f20ebbe9-5619-4870-927b-8e5e4bdecb66-720.m3u8",
-		      label: 'HD',
-		      'type': 'mp4',
+		    sources: [{file: "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/857470f0-85b7-45df-838c-9cf001451114-240.m3u8",  type:"mp4",label:"240p", default: "true"},
+      {file: "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/65f033fc-a2c4-44ad-8c1e-3fe081b6af80-480.m3u8",  type:"mp4", label:"480p"},
+      {file: "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/f20ebbe9-5619-4870-927b-8e5e4bdecb66-720.m3u8", type:"mp4", label:"720p"}
 		      primary: 'html5',
 
 		    },
