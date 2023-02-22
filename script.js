e1c5b99866908dbf626d6b189d75c92d7c979244
Lionelp0 <125425598+Lionelp0@users.noreply.github.com>
2023-02-22 13:29:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -22,37 +22,38 @@ jwplayer("container").setup({
 
 		  playlist: [
         ,
-        //Begin Movies Truyen Ky Ly Tieu Long Tap 1
+
        {        
-		    title: "   ",
-		    description: "   ",
-		    image: "   ",
-		     sources: [{
-		     {
-          file:
-            "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/65f033fc-a2c4-44ad-8c1e-3fe081b6af80-480.m3u8",
-          label: "480p",
-          default: true
-        },
-        {
-          file:
-            "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/857470f0-85b7-45df-838c-9cf001451114-240.m3u8",
-          label: "240p"
-        },
-        {
-          file:
-            "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/65f033fc-a2c4-44ad-8c1e-3fe081b6af80-480.m3u8",
-          label: "480p"
-        }, {
-          file:
-            "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/f20ebbe9-5619-4870-927b-8e5e4bdecb66-720.m3u8",
-          label: "720p",
-          default: true
-        },		    
-],
+		    title: "Truyền Kỳ Lý Tiểu Long Tập 01 720P VietSub EngSub",
+		    description: "Truyền Kỳ Lý Tiểu Long Tập 01 720P VietSub EngSub",
+		    image: "https://cdn.vietsubtv.org/images/film/truyen-ky-ly-tieu-long-5726.jpg",
+		    sources: [{
+		    file: "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/65f033fc-a2c4-44ad-8c1e-3fe081b6af80-480.m3u8",
+		      label: '480',
+		      'type': 'mp4',
+		      primary: 'html5',
+
+		    },{
+		    file: "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/857470f0-85b7-45df-838c-9cf001451114-240.m3u8",
+		      label: '240p',
+		      'type': 'mp4',
+		      primary: 'html5',
+
+		    },{
+		     file: "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/65f033fc-a2c4-44ad-8c1e-3fe081b6af80-480.m3u8",
+		      label: '480p',
+		      'type': 'mp4',
+		      primary: 'html5',
+		    },{
+		     file: "https://peertube.otakufarms.com/static/streaming-playlists/hls/c4eeaadf-9d35-4238-8657-fdceb0c52d2e/f20ebbe9-5619-4870-927b-8e5e4bdecb66-720.m3u8",
+		      label: '720p',
+		      'type': 'mp4',
+		      primary: 'html5',
+		    }],
 		    
-		 
-		  }//end of movies
+		   
+		    
+		  }//end of movies 
       ]
 		});
 jwplayer("container").setCaptions({
